737aac69100d86c9ed5b56e4d2be197bb9b76367
Walter1215 <wcuad002@fiu.edu>
2026-05-27 21:59:19 -0400

    Add backend API routes and update READMe

--- README.md
+++ README.md
@@ -1,16 +1,40 @@
-# FlowFit Backend
+# FlowFit Backend API
 
-Backend API for the FlowFit fitness application built using Flask and PostgreSQL.
+Backend fitness tracking API built using Python Flask and RESTful routes. Implements workout data handling, JSON request/response workflows, and backend API development concepts.
 
-## Features
-- REST API development
-- Backend routing
-- Workout data management
+## Current Features
+
+* Flask backend setup
+* GET API routes
+* POST API routes
+* JSON request handling
+* Workout data storage
+* curl API testing
+
+### GET Routes
+
+* `/`
+* `/workouts`
+* `/users`
+
+### POST Routes
+
+* `/add-workout`
 
 ## Technologies
-- Python
-- Flask
-- PostgreSQL
+
+* Python
+* Flask
+* REST APIs
+* JSON
+
+## Future Improvements
+
+* PostgreSQL integration
+* Persistent database storage
+* User authentication
+* Workout tracking enhancements
+* Frontend/mobile integration
 
 ## Run
 
--- server.py
+++ server.py
@@ -1,18 +1,52 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+workouts = [
+    {
+        "name": "Pushups",
+        "sets": 3
+    },
+    {
+        "name": "Squats",
+        "sets": 4
+    },
+
+    {
+        "name": "PullUps",
+        "sets": 5
+    }
+]
 
 @app.route("/")
 def home():
     return {"message": "FlowFit backend running"}
 
 @app.route("/workouts")
-def workouts():
+def get_workouts():
+    return {"workouts": workouts}
+
+@app.route("/add-workout", methods=["POST"])
+def add_workout():
+    data = request.json
+
+    print(data)
+
+    workouts.append(data)
+
     return {
-        "workouts": [
-            "Push Ups",
-            "Squats",
-            "Running"
+        "message": "Workout added",
+        "workouts": workouts
+    }
+
+@app.route("/users")
+def users():
+    return {
+        "users": [
+        "Walter",
+        "Alex",
+        "Lucilla"
+
         ]
     }
 
